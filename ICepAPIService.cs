cd8ad09224a2a278c60f7e527ebfceaadd4831f7
Rafael Lira <rafael.alexandrelira@hotmail.com>
2020-10-08 13:45:41 -0300

    Adicionar arquivos de projeto.

---
+++ ICepAPIService.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI
+{
+    public interface ICepAPIService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+         
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI
+{
+    class Program
+    {       //Estamos usando uma tarefa asssincrona com async task no método Main
+        static async Task Main(string[] args)
+        {
+            string CepInformado;          
+
+            try
+            {                   
+                var CepClient = RestService.For<ICepAPIService>("http://viacep.com.br");
+
+                Console.WriteLine("ISTO APENAS É UM TESTE\n");
+
+                Console.WriteLine("Informe o CEP que deseja consultar: ");
+                CepInformado = Console.ReadLine();
+
+                var Address = await CepClient.GetAddressAsync(CepInformado);
+
+                Console.WriteLine("ISTO APENAS É UM TESTE \n");
+                Console.WriteLine("\nLogradouro: {0}\nBairro: {1}\nCidade: {2}",Address.Logradouro,Address.Bairro, Address.Localidade);
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ISTO APENAS É UM TESTE \n");
+                Console.WriteLine("Erro ao consultar CEP, Verifique os campos e tente novamente "+ e.Message);
+            }
+
+        }
+    }
+}
